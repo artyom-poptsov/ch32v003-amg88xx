8ecefbee2d5dcd1f9b7a72e82cabbe9c9bc1563f
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2026-06-09 22:00:07 +0300

    .dir-locals.el: Add to the repository

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; .dir-locals.el -- Per-directory local variables for GNU Emacs 23 and later.
+
+((nil         . ((fill-column  . 78)
+                 (tab-width    .  8)))
+ (c-mode . ((indent-tabs-mode . nil)
+            (c-basic-offset . 8)
+            (c-file-style . "linux"))))
+
+;;; .dir-locals.el ends here
+
